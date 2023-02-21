c0a92a3bf9907766ef5f3974676181875b20fffd
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-02-21 10:18:05 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'pages/share_plus_page.dart';
+import 'my_app.dart';
 
 void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Share Plus Plugin Demo',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorSchemeSeed: const Color(0x9f4376f8),
-          appBarTheme: const AppBarTheme(
-            titleTextStyle: TextStyle(fontSize: 30),
-            backgroundColor: Colors.orange,
-          ),
-          textTheme: const TextTheme(
-            bodyLarge: TextStyle(fontSize: 30.0),
-            titleMedium: TextStyle(fontSize: 30.0),
-            labelLarge: TextStyle(fontSize: 25.0),
-          ),
-        ),
-        home: const SharePlusPage(),
-      );
-}
